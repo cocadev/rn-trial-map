a95907729ef37e7cd74af44a45ace89578c8a1ad
kingofdevs <40970351+kingofdevs@users.noreply.github.com>
2020-04-01 03:37:02 +0800

    add fonts

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import {
   SafeAreaView,
   StatusBar,
-  TouchableOpacity,
   Text,
   View,
   Image,
@@ -15,6 +14,7 @@ import MapView, { Callout, Marker } from 'react-native-maps';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Feather from 'react-native-vector-icons/Feather';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
 
 const { width, height } = Dimensions.get('window');
 const PLACES = [
@@ -94,13 +94,13 @@ const App = () => {
           style={styles.backHeader}
         >
           <Text style={styles.text1}>Space within 5 miles of</Text>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', elevation: 3 }}>
             <View style={{ flexDirection: 'row' }}>
-              <MaterialCommunityIcons name="map-marker" size={18} style={{ opacity: 0.5 }} />
+              <MaterialCommunityIcons name="map-marker" size={18} style={{ opacity: 0.5, marginLeft: -5 }} />
               <Text style={styles.text2}>buckingham palace, westminster, lon…</Text>
             </View>
 
-            <View style={{ flexDirection: 'row' }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
               <MaterialCommunityIcons name="lead-pencil" size={18} color={'#0029ff'} />
               <Text style={styles.text3}>Modify</Text>
             </View>
@@ -121,7 +121,7 @@ const App = () => {
             scrollEnabled={nestedScrollEnabled}
             style={{
               width: '100%',
-              maxHeight: 500,
+              height: 400,
               padding: 20,
               paddingRight: 0
             }}>
@@ -149,7 +149,44 @@ const App = () => {
               <Image source={require('./assets/demo2.jpg')} style={styles.img}/>
               <Image source={require('./assets/demo2.jpg')} style={styles.img}/>
             </ScrollView>
+
+            <Text style={styles.text8}>Space Available</Text>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 8}}>
+                <Text style={styles.text2}>7 Desks available</Text>
+                <Text style={styles.text9}>£350 / person</Text>
+            </View>
+
+            <Text style={styles.text8}>Amenities</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 13}}>
+              <EvilIcons name="check" size={18} />
+              <Text style={[styles.text2, { marginLeft: 3}]}>Free Wifi</Text>
+            </View>
+
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 13}}>
+              <EvilIcons name="check" size={18} />
+              <Text style={[styles.text2, { marginLeft: 20}]}>Coffee & Tea Machines</Text>
+            </View>
+
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 13}}>
+              <EvilIcons name="check" size={18} />
+              <Text style={[styles.text2, { marginLeft: 3}]}>Meeting Rooms</Text>
+            </View>
+
+            <View style={{ height: 60}}/>
+
           </ScrollView>
+
+          <View style={styles.bottom}>
+            <View style={styles.chatBtn}>
+              <Image source={require('./assets/chat.png')} style={{ width:15, height: 13}}/>
+              <Text style={styles.text10}>Chat</Text>
+            </View>
+
+            <View style={styles.bookBtn}>
+              <Text style={styles.text11}>Book Now</Text>
+            </View>
+            
+          </View>
 
         </ActionSheet>
       </SafeAreaView>
@@ -181,7 +218,10 @@ const styles = StyleSheet.create({
     width,
     height: 64,
     paddingHorizontal: 20,
-    paddingVertical: 14
+    paddingVertical: 14,
+    borderRadius: 20,
+    elevation: 3,
+    overflow: 'hidden',
   },
   text1: {
     opacity: 0.3,
@@ -198,13 +238,13 @@ const styles = StyleSheet.create({
   },
   text3: {
     color: '#0029ff',
-    fontFamily: 'Rubik Medium',
+    fontFamily: 'Rubik-Medium',
     fontSize: 12,
     fontWeight: '500',
     marginLeft: 3
   },
   text4: {
-    fontFamily: 'Rubik Medium',
+    fontFamily: 'Rubik-Medium',
     fontSize: 20,
     fontWeight: '500',
     marginBottom: 8
@@ -233,5 +273,71 @@ const styles = StyleSheet.create({
     borderRadius: 6,
     marginRight: 12,
     marginVertical: 13
+  },
+  text8: {
+    color: '#000000',
+    fontFamily: 'Rubik-Medium',
+    fontSize: 16,
+    fontWeight: '500',
+    marginTop: 14
+  },
+  text9: {
+    fontFamily: 'Rubik-Medium',
+    fontSize: 13,
+    fontWeight: '500',
+    marginRight: 20
+  },
+  bottom: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    shadowColor: 'rgba(71, 77, 96, 0.1)',
+    shadowOffset: { width: 3, height: 0 },
+    shadowRadius: 6,
+    shadowColor: 'rgba(0, 0, 0, 0.1)',
+    shadowOffset: { width: -4, height: 4 },
+    shadowRadius: 15,
+    backgroundColor: '#fff',
+    position: 'absolute',
+    bottom: 0,
+    height: 60,
+    width,
+    elevation: 5
+  },
+  text10: {
+    fontFamily: 'Rubik',
+    fontSize: 14,
+    fontWeight: '400',
+    lineHeight: 16,
+    marginLeft: 5
+  },
+  chatBtn: {
+    flexDirection: 'row', 
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 20,
+    borderRadius: 6,
+    borderColor: '#000000',
+    borderStyle: 'solid',
+    borderWidth: 2,
+    backgroundColor: '#ffffff',
+    width: 109,
+    height: 40,
+  },
+  bookBtn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 6,
+    backgroundColor: '#0029ff',
+    flex: 1,
+    marginLeft: 8,
+    marginRight: 20,
+    height: 40,
+  },
+  text11: {
+    color: '#ffffff',
+    fontFamily: 'Rubik Medium',
+    fontSize: 14,
+    fontWeight: '500',
+    lineHeight: 16,
   }
 })
